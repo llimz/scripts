cfe6b4e2bb522b06be00eb05f8db2de23e3b4c6a
llimz <vdb.jimmy@gmail.com>
2017-03-15 10:58:39 +0100

    test

--- diskimage.sh
+++ diskimage.sh
@@ -1,4 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+
+if [[ -z $1 && -z $2 ]]; 
+then
+ echo 'usage diskimage.sh +image_file_name_without_extension'
+ exit 1
+fi
 
 if [ $(dpkg-query -W -f='${Status}' sshfs 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
@@ -12,6 +18,6 @@ sshfs -o noatime,noexec,allow_other root@192.168.0.7:/mnt/HD/HD_a2/Backup/ $MOUN
 sleep 5
 
 #pv -tpreb /dev/mmcblk0p2 | dd bs=1M | gzip -c > $MOUNT/rasprdc.img.gz
-dd if=/dev/mmcblk0p2 bs=1M | gzip -c > $MOUNT/rasprdc.img.gz
+dd if=/dev/mmcblk0p2 bs=1M | gzip -c > $MOUNT/$1.img.gz
 
 fusermount -u $MOUNT
